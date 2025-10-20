a00993a682c1ab1d2012f0eaceaaba8f0a7b7cb4
amardhadbale <amardhadbale@gmail.com>
2025-10-20 17:30:06 +0000

    first commit

---
+++ lib/Repository/Splash_screen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'Login_Screen.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(
+          
+        ),
+      ),
+    );
+  }
+}
